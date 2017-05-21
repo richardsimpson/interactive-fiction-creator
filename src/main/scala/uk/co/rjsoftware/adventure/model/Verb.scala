d87e3236e9cd4ffa42752c810e6d5d8df01b11ac
Richard Simpson <richard.simpson@king.com>
2017-05-21 23:17:11 +0100

    Removed a TODO

--- src/main/scala/uk/co/rjsoftware/adventure/model/Verb.scala
+++ src/main/scala/uk/co/rjsoftware/adventure/model/Verb.scala
@@ -3,7 +3,6 @@ package uk.co.rjsoftware.adventure.model
 /**
   * Created by richardsimpson on 20/05/2017.
   */
-// TODO: Does this class change the value of synonyms in the calling code?
 class Verb(private val synonyms:List[String]) {
 
     def getVerb : String = {
