73ad91d81357d878db394e4b92f2cf6e9d4104b5
Richard Simpson <richard.simpson@king.com>
2017-05-30 23:19:37 +0100

    Removed a TODO: no action required.

--- src/main/scala/uk/co/rjsoftware/adventure/model/CustomVerb.scala
+++ src/main/scala/uk/co/rjsoftware/adventure/model/CustomVerb.scala
@@ -3,7 +3,6 @@ package uk.co.rjsoftware.adventure.model
 /**
   * Created by richardsimpson on 20/05/2017.
   */
-// TODO: Note that the subclass constructor params that are passed through DO NOT have val or var before them.  Update the other classes like this.
 class CustomVerb(synonyms:List[String]) extends Verb(synonyms) {
 
     def this(name:String) {
