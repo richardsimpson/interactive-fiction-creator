5d423caa6b7a83fdf530e701d16c68ed2a91c9f7
richardsimpson <richard.level42@gmail.com>
2019-03-22 22:43:25 +0000

    Changed background of editor components to be white.

--- src/main/groovy/uk/co/rjsoftware/adventure/view/editor/components/CustomComponent.groovy
+++ src/main/groovy/uk/co/rjsoftware/adventure/view/editor/components/CustomComponent.groovy
@@ -1,6 +1,8 @@
 package uk.co.rjsoftware.adventure.view.editor.components
 
 import groovy.transform.TypeChecked
+import javafx.scene.layout.Background
+import javafx.scene.layout.BackgroundFill
 import javafx.scene.layout.Border
 import javafx.scene.layout.BorderStroke
 import javafx.scene.layout.BorderStrokeStyle
@@ -13,5 +15,6 @@ abstract class CustomComponent extends VBox {
     CustomComponent() {
         super(8)
         this.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, null, null)))
+        this.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)))
     }
 }
